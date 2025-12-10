557e9a05a90124afc1ce7aaccec771a6b6addc14
Jorge Matos <45945081+taleex@users.noreply.github.com>
2025-12-10 18:03:05 +0000

    Fetching improvements

--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -1,52 +1,35 @@
 import { useEffect, useState } from "react";
 import { jobItem, jobItemExpanded } from "./types";
 import { BASE_API_URL } from "./constants";
+import { useQuery } from "@tanstack/react-query";
 
+type JobItemAPIResponse = {
+  public: boolean;
+  jobItem: jobItemExpanded;
+};
 
-export function useActiveID(){
-
-        const [activeID, setActiveID] = useState<number | null>(null);
-      
-        useEffect(() => {
-      
-          const handleHashChange = () => {
-           const id =  +window.location.hash.slice(1);
-           setActiveID(id || null);
-          };
-      
-          handleHashChange();
-      
-          window.addEventListener("hashchange", handleHashChange);
-        
-          return () => {
-            window.removeEventListener("hashchange", handleHashChange);
-          }
-      
-        },[]);
-      
-      return activeID;
-}
-
-export function useJobItem(id: number | null) {
-      
-  const [JobItem, setJobItem] = useState<jobItemExpanded | null >(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-
-    if(!id) return;
-
-    const fetchData = async () => {
-      setIsLoading(true);
+const fetchJobItem = async (id: number): Promise<JobItemAPIResponse> =>  {
       const response = await fetch(`${BASE_API_URL}/${id}`);
       const data = await response.json();
-      setIsLoading(false);
-      setJobItem(data.jobItem);
+      return data;
     };
 
-    fetchData();
+export function useJobItem(id: number | null) {
+ 
+  const {data, isInitialLoading} = useQuery(
+    ['job-item', id],
+    () => (id ? fetchJobItem(id) : null), 
+    {
+      enabled: Boolean(id),
+      staleTime: 1000 * 60 * 60,
+      refetchOnWindowFocus: false,
+      retry: false,
+      onError: () => { }
+    }
+  );
 
-  }, [id]);
+  const JobItem = data?.jobItem
+  const isLoading = isInitialLoading;
 
   return { JobItem, isLoading } as const;
 }
@@ -88,4 +71,28 @@ export function useDebounced<T>(value: T, delay = 500): T {
       }, [value, delay]);
 
       return debouncedValue;
+}
+
+export function useActiveID(){
+
+        const [activeID, setActiveID] = useState<number | null>(null);
+      
+        useEffect(() => {
+      
+          const handleHashChange = () => {
+           const id =  +window.location.hash.slice(1);
+           setActiveID(id || null);
+          };
+      
+          handleHashChange();
+      
+          window.addEventListener("hashchange", handleHashChange);
+        
+          return () => {
+            window.removeEventListener("hashchange", handleHashChange);
+          }
+      
+        },[]);
+      
+      return activeID;
 }
